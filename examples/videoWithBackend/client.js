e600def51c1d83aeac913f494c5b7c755869db85
Jussi Rytkönen <jussi.rytkonen@gmail.com>
2021-02-03 12:58:54 +0200

    videoWithBackend show wallet error states

--- examples/videoWithBackend/client.js
+++ examples/videoWithBackend/client.js
@@ -2,19 +2,48 @@ import '/packages/wmm-web-components/videoComponent.js'
 import { logEvent } from '/packages/wmm-utils/client/index.js'
 import {fullPaymentUrl} from './config.js'
 
-const info = document.createElement('div')
+const info = document.querySelector('#logs')
+
 // Add video element to the dom
 const wmmVideo = document.createElement('wmm-video')
 wmmVideo.src = `http://localhost:3009/videoFile`
 wmmVideo.paymentUrl = fullPaymentUrl
-// wmmVideo.setAttribute('skipVerification', true)
-// Log events
-wmmVideo.addEventListener('monetized', ({detail}) =>
-  logEvent(`Balance: ${detail.accountBalance}`, info))
-wmmVideo.addEventListener('monetizeFailed', ({detail}) =>
-  logEvent(`Failed: ${detail}`, info))
-wmmVideo.addEventListener('play', ev => console.log('play', ev))
 
+const videoEvents = {
+  "monetized": ({detail}) =>
+    logEvent(`Balance: ${detail.accountBalance}`, info),
+  "monetizeFailed": ({detail}) =>
+    logEvent(`Failed: ${detail}`, info),
+  "play": ev =>
+    console.log('play', ev),
+  "stalled": () => {
+    // TODO create non intrusive info
+    if (!document.monetization) {
+      alert("Monetization failed in error state.")
+    } else {
+      switch (document.monetization.state) {
+        case 'stopped':
+          alert("Install Coil wallet to start paying for video content to see more.")
+          break;
+        case 'pending':
+          alert("Failed to start monetization, even though wallet installed.")
+          break;
+        case 'started':
+          alert("Monetization running, but not enough payments to show the video.")
+          break;
+      }
+    }
+  },
+  "wallet but not enough payments": () => {
+    console.log()
+  }
+}
+
+for (let [evName, action] of Object.entries(videoEvents)) {
+  wmmVideo.addEventListener(evName, action)
+}
+
+// Use setTimeout to work well with docusaurus
 setTimeout(() => {
   const container = document.querySelector('#video-container')
   container.textContent = ''
